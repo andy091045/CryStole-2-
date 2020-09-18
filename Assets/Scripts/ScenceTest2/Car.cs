b52fb08ff73c2a8853e2da6aa9fd8fe796d4f337
Andy SHU <andy091045@gmail.com>
2020-09-18 16:33:59 +0800

    remindCube

--- Assets/Scripts/ScenceTest2/Car.cs
+++ Assets/Scripts/ScenceTest2/Car.cs
@@ -6,7 +6,7 @@ public class Car : MonoBehaviour
 {
     public GameObject[] wayPoints;
     private int index = 0;
-    private float speed = 10;
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
