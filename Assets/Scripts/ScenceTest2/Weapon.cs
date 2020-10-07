8f3e5fa8f29a5e943880809c9c924f1c9f4e9c9f
Andy SHU <andy091045@gmail.com>
2020-10-07 22:57:39 +0800

    debug

--- Assets/Scripts/ScenceTest2/Weapon.cs
+++ Assets/Scripts/ScenceTest2/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public PlayerType playerType;
     public GameObject Sword;
 
     public GameObject NextSword;
@@ -15,8 +16,8 @@ public class Weapon : MonoBehaviour
     AudioSource audiosource;
     // public Score andy;
     // public Numbers amy;
-    public LayerMask layer;
-    private Vector3 previousPos;
+    // public LayerMask layer;
+    // private Vector3 previousPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,18 +26,32 @@ public class Weapon : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-
-        if (other.name == "Controller (right)" || other.name == "Controller (left)")
+        //左手武器只能攻擊左手屬性敵人
+        if (playerType == PlayerType.LeftType)
         {
-            Debug.Log("Change!");
-            Sword.SetActive(false);
-            NextSword.SetActive(true);
-
-            changeSword = Sword;
-            Sword = NextSword;
-            NextSword = changeSword;
-
+            if (other.gameObject.CompareTag("LeftEnemy"))
+            {
+                Destroy(other.gameObject);
+            }
         }
+        //右手武器只能攻擊右手屬性敵人
+        if (playerType == PlayerType.RightType)
+        {
+            if (other.gameObject.CompareTag("RightEnemy"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+
+        // if (other.name == "Controller (right)" || other.name == "Controller (left)")
+        // {
+        //     Debug.Log("Change!");
+        //     Sword.SetActive(false);
+        //     NextSword.SetActive(true);
+        //     changeSword = Sword;
+        //     Sword = NextSword;
+        //     NextSword = changeSword;
+        // }
     }
     // Update is called once per frame
     void Update()
@@ -50,21 +65,22 @@ public class Weapon : MonoBehaviour
             NextSword = changeSword;
             audiosource.PlayOneShot(impact);
         }
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
-        {
-            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 0)
-            {
-                audiosource.PlayOneShot(impact);
-                Debug.Log("music");
-                Lean.Pool.LeanPool.Despawn(hit.transform.gameObject);
-                // andy.distroyCube();
-                // amy.distroyCube();
-
-            }
-        }
-        previousPos = transform.position;
-
-
+        // RaycastHit hit;
+        // if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        // {
+        //     if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 0)
+        //     {
+        //         audiosource.PlayOneShot(impact);
+        //         Debug.Log("music");
+        //         Lean.Pool.LeanPool.Despawn(hit.transform.gameObject);
+        //         // andy.distroyCube();
+        //         // amy.distroyCube();
+        //     }
+        // }
+        // previousPos = transform.position;
+    }
+    public enum PlayerType
+    {
+        LeftType, RightType
     }
 }
