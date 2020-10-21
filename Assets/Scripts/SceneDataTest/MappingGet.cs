f4668864185ff4793240445c3d63bfe2c600f2c1
Andy SHU <andy091045@gmail.com>
2020-10-21 16:09:17 +0800

    way remake

---
+++ Assets/Scripts/SceneDataTest/MappingGet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MappingGet : MonoBehaviour
+{
+    public TextAsset mapping;
+    Music data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = JsonUtility.FromJson<Music>(mapping.text);
+        Debug.Log(data._events[0]._time);
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            data._events[0]._time += 1;
+            string path = Application.persistentDataPath + "/data.json";
+            FileStream fs = new FileStream(path, FileMode.Create);
+            string fileContext = JsonUtility.ToJson(data);
+            StreamWriter file = new StreamWriter(fs);
+            file.Write(fileContext);
+            file.Close();
+        }
+    }
+
+    // Update is called once per frame
+}
+[System.Serializable]
+public class Events
+{
+    public double _time;
+    public int _type;
+    public int _value;
+}
+
+[System.Serializable]
+public class Music
+{
+    public Events[] _events;
+}
