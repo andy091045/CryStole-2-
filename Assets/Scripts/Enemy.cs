b5756bf3761144bf036f71c1d0ba5f1a67fe3539
Andy SHU <andy091045@gmail.com>
2020-12-08 01:21:37 +0800

    怪特效測試

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // [Header("Unity Setup")]
+    public ParticleSystem deathParticles;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Wall"))
+        {
+            Destroy();
+        }
+    }
+    public void Destroy()
+    {
+        Instantiate(deathParticles, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
