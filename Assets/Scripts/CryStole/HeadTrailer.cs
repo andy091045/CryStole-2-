4826fccc5b313441c8359c5739b2a806e4f2b624
Andy SHU <andy091045@gmail.com>
2020-12-07 16:50:52 +0800

    頭盔製作中

---
+++ Assets/Scripts/CryStole/HeadTrailer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTrailer : MonoBehaviour
+{
+    public GameObject targetPosition;   // 目標位置
+    Vector3 currentVelocity = Vector3.zero;     // 當前速度，這個值由你每次呼叫這個函式時被修改
+    float maxSpeed = 10f;    // 選擇允許你限制的最大速度
+    float smoothTime = 0.01f;      // 達到目標大約花費的時間。 一個較小的值將更快達到目標。
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition.transform.position, ref currentVelocity, smoothTime, maxSpeed);
+    }
+}
