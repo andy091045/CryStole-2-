3fd36868be4fc3c81549d39778707a23ac81cfa4
Andy <andy091045@gmail.com.tw>
2020-12-10 14:18:22 +0800

    明天報告用

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -8,6 +8,7 @@ public class ChangeScene : MonoBehaviour
         public void changeScene(string sceneName){
         Application.LoadLevel(sceneName);
         Debug.Log("ChangeScene");
+        Debug.Log("GIT測試");
         //重新設定遊玩
         CheckUserInput.Instance.ResumeGame();
     }    
