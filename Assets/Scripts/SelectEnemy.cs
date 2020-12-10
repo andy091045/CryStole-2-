72943aab6aa3cc663dfa08765d490166a021d41c
Andy SHU <andy091045@gmail.com>
2020-12-11 00:59:20 +0800

    自己排版

---
+++ Assets/Scripts/SelectEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+public class SelectEnemy : MonoBehaviour
+{
+    public EnemyType enemyType;
+    public GameObject PurpleEnemy;
+    public GameObject BlueEnemy;
+    public GameObject YellowEnemy;
+    public GameObject RedEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (enemyType == EnemyType.BlueType)
+        {
+            GameObject cube = Lean.Pool.LeanPool.Spawn(BlueEnemy, this.transform);
+        }
+        else if (enemyType == EnemyType.PurpleType)
+        {
+            GameObject cube = Lean.Pool.LeanPool.Spawn(PurpleEnemy, this.transform);
+        }
+        else if (enemyType == EnemyType.RedType)
+        {
+            GameObject cube = Lean.Pool.LeanPool.Spawn(RedEnemy, this.transform);
+        }
+        else
+        {
+            GameObject cube = Lean.Pool.LeanPool.Spawn(YellowEnemy, this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public enum EnemyType
+    {
+        PurpleType, YellowType, BlueType, RedType
+    }
+}
---
+++ Assets/Scripts/mappingPerSecond.cs
@@ -0,0 +1,35 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class mappingPerSecond : MonoBehaviour
+// {
+//     public GameObject[] cubes;
+//     public bool zero = false;
+//     public bool one = false;
+//     public bool two = false;
+//     public bool three = false;
+//     public bool four = false;
+//     public bool five = false;
+
+//     public bool six = false;
+//     public bool seven = false;
+//     public bool eight = false;
+//     public bool nine = false;
+//     public bool ten = false;
+//     public bool eleven = false;
+
+//     void Start()
+//     {
+//         for (int i = 0; i <= 11; i++)
+//         {
+//             cubes[i].SetActive(false);
+//         }
+//         if (zero == true)
+//         {
+//             cubes[0].SetActive(true);
+//             Debug.Log("0 true");
+//         }
+//     }
+
+// }
