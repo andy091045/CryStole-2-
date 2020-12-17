01ec567f778599c1d4a6a0e49d90c85f21792c25
Andy SHU <andy091045@gmail.com>
2020-12-17 21:35:53 +0800

    TMP

--- Assets/Scripts/CryStole/MappingCreator.cs
+++ Assets/Scripts/CryStole/MappingCreator.cs
@@ -17,25 +17,25 @@ public class MappingCreator : MonoBehaviour
     void Start()
     {
         data = JsonUtility.FromJson<Music2>(mapping.text);
-        for (int i = 0; i < 187; i++)
+        for (int i = 0; i <= 111; i++)
         {
             if (data._notes[i]._type == 0)
             {
                 GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[0], points[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
                 Vector3 move = cube.transform.position;
-                move = new Vector3(move.x, move.y, move.z + 10.0f * (data._notes[i]._time + 0.5f));
+                move = new Vector3(move.x, move.y, move.z + 10.0f * (data._notes[i]._time * 0.46f));
                 if (move.z > 100.0f)
                 {
                     move.y += 25.0f;
                 }
-                Debug.Log("紅方塊");
+                Debug.Log(i + ":" + move);
                 cube.transform.position = move;
             }
             else if (data._notes[i]._type == 1)
             {
                 GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[1], points[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
                 Vector3 move = cube.transform.position;
-                move = new Vector3(move.x, move.y, move.z + 1.0f * data._notes[i]._time);
+                move = new Vector3(move.x, move.y, move.z + 10.0f * (data._notes[i]._time * 0.46f));
                 if (move.z > 100.0f)
                 {
                     move.y += 25.0f;
@@ -46,7 +46,7 @@ public class MappingCreator : MonoBehaviour
             {
                 GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[2], points[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
                 Vector3 move = cube.transform.position;
-                move = new Vector3(move.x, move.y, move.z + 1.0f * data._notes[i]._time);
+                move = new Vector3(move.x, move.y, move.z + 10.0f * (data._notes[i]._time * 0.46f));
                 if (move.z > 100.0f)
                 {
                     move.y += 25.0f;
@@ -57,7 +57,7 @@ public class MappingCreator : MonoBehaviour
             {
                 GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[3], points[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
                 Vector3 move = cube.transform.position;
-                move = new Vector3(move.x, move.y, move.z + 1.0f * data._notes[i]._time);
+                move = new Vector3(move.x, move.y, move.z + 10.0f * (data._notes[i]._time * 0.46f));
                 if (move.z > 100.0f)
                 {
                     move.y += 25.0f;
@@ -66,35 +66,62 @@ public class MappingCreator : MonoBehaviour
             }
         }
 
-        // for (int i = 187; i < 1000; i++)
-        // {
-        //     if (data._notes[i]._value <= 3)
-        //     {
-        //         GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[0], points2[data._notes[i]._type]);
-        //         Vector3 move = cube.transform.position;
-        //         move = new Vector3((move.x + 10 * data._notes[i]._time) - 431.0f, move.y, move.z);
-        //         if (move.x > 235.0f)
-        //         {
-        //             move.y -= 25.0f;
-        //         }
-        //         cube.transform.position = move;
-        //         //物體旋轉
-        //         cube.transform.Rotate(0, 90, 0);
-        //     }
-        //     else
-        //     {
-        //         GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[1], points2[data._notes[i]._type]);
-        //         Vector3 move = cube.transform.position;
-        //         move = new Vector3((move.x + 10 * data._notes[i]._time) - 431.0f, move.y, move.z);
-        //         if (move.x > 235.0f)
-        //         {
-        //             move.y -= 25.0f;
-        //         }
-        //         cube.transform.position = move;
-        //         //物體旋轉
-        //         cube.transform.Rotate(0, 90, 0);
-        //     }
-        // }
+        for (int i = 112; i < 1000; i++)
+        {
+            // Debug.Log(i + ":" + data._notes[i]._type);
+            if (data._notes[i]._type == 0)
+            {
+                GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[0], points2[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
+                Vector3 move = cube.transform.position;
+                move = new Vector3((move.x + 10 * data._notes[i]._time * 0.46f) - 431.0f, move.y, move.z);
+                if (move.x > 235.0f)
+                {
+                    move.y -= 25.0f;
+                }
+                cube.transform.position = move;
+                //物體旋轉
+                cube.transform.Rotate(0, 90, 0);
+            }
+            else if (data._notes[i]._type == 1)
+            {
+                GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[1], points2[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
+                Vector3 move = cube.transform.position;
+                move = new Vector3((move.x + 10 * data._notes[i]._time * 0.46f) - 431.0f, move.y, move.z);
+                if (move.x > 235.0f)
+                {
+                    move.y -= 25.0f;
+                }
+                cube.transform.position = move;
+                //物體旋轉
+                cube.transform.Rotate(0, 90, 0);
+            }
+            else if (data._notes[i]._type == 2)
+            {
+                GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[2], points2[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
+                Vector3 move = cube.transform.position;
+                move = new Vector3((move.x + 10 * data._notes[i]._time * 0.46f) - 431.0f, move.y, move.z);
+                if (move.x > 235.0f)
+                {
+                    move.y -= 25.0f;
+                }
+                cube.transform.position = move;
+                //物體旋轉
+                cube.transform.Rotate(0, 90, 0);
+            }
+            else
+            {
+                GameObject cube = Lean.Pool.LeanPool.Spawn(cubes[3], points2[data._notes[i]._lineIndex * 1 + data._notes[i]._lineLayer * 4]);
+                Vector3 move = cube.transform.position;
+                move = new Vector3((move.x + 10 * data._notes[i]._time * 0.46f) - 431.0f, move.y, move.z);
+                if (move.x > 235.0f)
+                {
+                    move.y -= 25.0f;
+                }
+                cube.transform.position = move;
+                //物體旋轉
+                cube.transform.Rotate(0, 90, 0);
+            }
+        }
     }
     private void Update()
     {
