c31a23be8b1c852b41ccb0245e78c9abb14be5b1
Andy SHU <andy091045@gmail.com>
2020-12-19 16:40:16 +0800

    修改1

---
+++ Assets/Scripts/CryStole/ChangeSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpeed : MonoBehaviour
+{
+    public SpeedType speedType;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("碰到了");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (speedType == SpeedType.AddSpeed)
+            {
+                CarType2.Instance.speed = 11.45f;
+            }
+            if (speedType == SpeedType.MinusSpeed)
+            {
+                CarType2.Instance.speed = 10;
+            }
+        }
+    }
+
+    public enum SpeedType
+    {
+        AddSpeed, MinusSpeed
+    }
+}
