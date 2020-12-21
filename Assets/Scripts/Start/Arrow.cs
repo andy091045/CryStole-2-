e8bc82cc1ed6d06ac3b7c3494f0077c84aae5696
Andy SHU <andy091045@gmail.com>
2020-12-21 14:09:26 +0800

    ui 1

---
+++ Assets/Scripts/Start/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public GameObject[] photos;
+    public ArrowType arrowType;
+    int pressNumber = 0;
+    public int MaxItemAmount = 0;
+    public void PressDownArrow()
+    {
+        if (arrowType == ArrowType.RightArrow)
+        {
+            if (pressNumber < MaxItemAmount)
+            {
+                photos[pressNumber].SetActive(false);
+                pressNumber++;
+                photos[pressNumber].SetActive(true);
+            }
+
+        }
+        if (arrowType == ArrowType.LeftArrow)
+        {
+            if (pressNumber > 0)
+            {
+                photos[pressNumber].SetActive(false);
+                pressNumber--;
+                photos[pressNumber].SetActive(true);
+            }
+        }
+    }
+    public enum ArrowType
+    {
+        LeftArrow, RightArrow
+    }
+}
---
+++ Assets/Scripts/Start/HideItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideItem : MonoBehaviour
+{
+    public string GameObjectName;
+    public GameObject[] HideItems;
+    public int MaxItemAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= MaxItemAmount; i++)
+        {
+            HideItems[i].SetActive(false);
+        }
+    }
+
+    public void OpenItems()
+    {
+        for (int i = 0; i <= MaxItemAmount; i++)
+        {
+            HideItems[i].SetActive(true);
+        }
+    }
+}
